c2810f3fed37750cf2d2a984ffd37f0665c56f20
PrajanBasnet <spiderbasnet140@gmail.com>
2024-10-03 09:09:19 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express =  require("express");
+const app = express();
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+app.get('/',(req,res) =>{
+    res.render("index",{title:'MxLinux'});
+})
+
+app.listen(3000);
