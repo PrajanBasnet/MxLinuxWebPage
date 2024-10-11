8b7901fe25846163f84e69fa66820c3b0e8e3d08
hater <PrajanBasnet>
2024-10-11 23:02:17 +0545

    working code

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ const app = express();
 const { MongoClient } = require('mongodb');
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 
 // Database code 
 
-async function main() {
-    const url = "mongodb+srv://haterh:prajwal991@cluster0.elv1j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    const client = new MongoClient(url);
-    try {
+app.post('/blog',(req,res) =>{
+
+    async function main() {
+        const url = "mongodb+srv://HaterX:prajwal991@hater.oeu4l.mongodb.net/?retryWrites=true&w=majority&appName=Hater";
+        const client = new MongoClient(url);
+        try {
+
+        const author = req.body.Author;
+        const myBody = req.body.Body;
+        const title = req.body.Title;
         await client.connect();
-        await listDatabases(client);
+        const myDB = client.db("spider");
+        const myColl = myDB.collection("hero");
+        const doc = {title:`${title}`
+        , body:myBody,
+            author:author
+        };
+        const result = await myColl.insertOne(doc);
+        // await listDatabases(client);
+        res.render("news",{title:"news"});
     } catch (err) {
         console.log(e);
     } finally {
@@ -23,22 +37,20 @@ async function main() {
 
 main().catch(console.error);
 
-async function listDatabases(client) {
-    const databaseList = await client.db().admin().listDatabases();
-    console.log("databaseList");
-    databaseList.databases.forEach(db => {
-        console.log(`- ${db.name}`);
-    })
-}
 
-//database ends here
-//route code
+
+
 app.get('/', (req, res) => {
     res.render("index", { title: 'MxLinux' });
 })
 app.get('/news', (req, res) => {
     res.render("news", { title: 'about' });
 })
+app.get('/blog',(req,res) =>{
+    res.render("create",{title:'blogs'});
+})
+
+
 app.listen((3000), () => {
     console.log("server is running on http://127.0.0.1:3000");
 });
