d23f1aed797a610fbc09d2c8b2043bbe84928e79
hater <PrajanBasnet>
2024-10-14 19:53:52 +0545

    finally

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,48 +1,42 @@
 const express = require("express");
-const { route } = require("./test");
-const app =express();
-app.use(express.urlencoded());
+const { MongoClient } = require("mongodb");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 const routerDB = express.Router();
 
-routerDB.post('/blog',(req,res) =>{
+routerDB.post('/blog', (req, res) => {
     async function main() {
-
         const url = "mongodb+srv://HaterX:prajwal991@hater.oeu4l.mongodb.net/?retryWrites=true&w=majority&appName=Hater";
+
         const client = new MongoClient(url);
         try {
+            await client.connect();
+            const author = req.body.author;
+            const myBody = req.body.body; // Assuming 'back' was a typo
+            const title = req.body.title;
 
-        const author = req.body.Author;
-        const myBody = req.body.Body;
-        const title = req.body.Title;
-        await client.connect();
-        const myDB = client.db("spider");
-        const myColl = myDB.collection("hero");
-        const doc = {title:`${title}`
-        , body:myBody,
-            author:author
-        };
-        const result = await myColl.insertOne(doc);
-        // await listDatabases(client);
-        res.render("news",{title:"news"});
-    } catch (err) {
-        console.log(e);
-    } finally {
-        await client.close();
+            const myDB = client.db("spider");
+            const myColl = myDB.collection("hero");
+            const doc = {
+                title: title,
+                body: myBody,
+                author: author
+            };
+            const result = await myColl.insertOne(doc);
+            res.render("news", { title: "news" });
+        } catch (err) {
+            console.error("There was an error:", err);
+            res.status(500).send("Internal Server Error");
+        } finally {
+            await client.close();
+        }
     }
-}
     main().catch(console.error);
-    
 });
 
-routerDB.get('/blog',(req,res) =>{
-    res.render("create",{title:'blogs'});
-})
+routerDB.get('/blog', (req, res) => {
+    res.render("create", { title: 'blogs' });
+});
 
-routerDB.get('/read',(req,res) =>{
-    const options = {
-        projection: {_id:0 , field1:1},
-    };
-})
-
-module.exports = routerDB
+module.exports = routerDB;
--- routes/test.js
+++ routes/test.js
@@ -1,13 +1,49 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const { route } = require("./test");
 
-const PORT = 3000;
+const app =express();
+app.use(express.urlencoded());
 
-const router = express.Router();
+const routerDB = express.Router();
 
-router.get('/test',(req,res) =>{
-    res.send("working wow great");
+routerDB.post('/blog',(req,res) =>{
+    async function main() {
+    // const url = "mongodb+srv://haterh:prajwal991@cluster0.elv1j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    try {
+            const url = "mongodb+srv://HaterX:prajwal991@hater.oeu4l.mongodb.net/?retryWrites=true&w=majority&appName=Hater";
+            
+            const client = new MongoClient(url);
+        const author = req.body.Author;
+        const myBody = req.body.Body;
+        const title = req.body.Title;
+        await client.connect();
+        const myDB = client.db("spider");
+        const myColl = myDB.collection("hero");
+        const doc = {title:`${title}`
+        , body:myBody,
+            author:author
+        };
+        const result = await myColl.insertOne(doc);
+        // await listDatabases(client);
+        res.render("news",{title:"news"});
+    } catch (err) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
+}
+    main().catch(console.error);
+    
+});
+
+routerDB.get('/blog',(req,res) =>{
+    res.render("create",{title:'blogs'});
 })
 
-app.use(router);
-module.exports = router;
+routerDB.get('/read',(req,res) =>{
+    const options = {
+        projection: {_id:0 , field1:1},
+    };
+})
+
+module.exports = routerDB
